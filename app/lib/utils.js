118e40883acc024b1287a5e27d97586a840d90d3
taylanacikgoz <acikgozta@gmail.com>
2023-12-24 01:32:07 +0300

    small fixes

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -5,7 +5,7 @@ const connection = {};
 export const connectToDB = async () => {
   try {
     if (connection.isConnected) return;
-    const db = await mongoose.connect(process.env.MONGO);
+    const db = await mongoose.connect(process.env.MONGODB_URI);
     connection.isConnected = db.connections[0].readyState;
   } catch (error) {
     throw new Error(error);
